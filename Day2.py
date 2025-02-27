5c30829eeb4789f762be72d491a46611f0b81688
shathiyakesavan <shathiyakesavan@gmail.com>
2025-02-27 17:36:18 -0500

    commit

---
+++ Day2.py
@@ -0,0 +1,22 @@
+"""Hello world
+This is a multiline comment
+"""
+# # print("Hello")
+# name = input("Please enter your name")
+# # print("Hello ", name)
+
+x = 5
+y = 3
+
+# x,y = y,x
+# print (x,y)
+x = x + y
+# y = x-y
+# x=x-y
+# print(x,y)
+
+# print(f"The value of x is {x}, and the value of y is {y}.")
+
+name = "vishnu"
+nubmers_of_characters_in_name = len(name)
+print(nubmers_of_characters_in_name)
